bbb95d6bb140b7ed9a74283e329a503f5d81d59a
NicholasFord98 <112104274+NicholasFord98@users.noreply.github.com>
2024-11-02 16:31:42 -0500

    Revert "Ford/player turn (#20)" (#21)
    This reverts commit 5a17c16a94b3547ee2c65ec50cbdffd5976bb574.

--- script/global.gd
+++
@@ -1,4 +0,0 @@
-extends Node
-
-var player_pos : Vector2
-var player_current_attack = false
--- script/hub.gd
+++
@@ -1,9 +0,0 @@
-extends Node2D
-
-@export var pause_menu_packed_scene : PackedScene = null
-@onready var canvas_layer = $CanvasLayer as CanvasLayer
-
-func _input(event):
-	if event.is_action("pause"):
-		var new_pause_menu : PauseMenu = pause_menu_packed_scene.instantiate()
-		canvas_layer.add_child(new_pause_menu)

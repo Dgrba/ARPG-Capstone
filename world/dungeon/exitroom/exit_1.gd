5c0e290b3aed4b42383c7ae4de23c8e8795414d6
NicholasFord98 <112104274+NicholasFord98@users.noreply.github.com>
2024-12-03 10:27:45 -0600

    Ford/working (#37)
    * Debuged dungeon rooms
    
    * fixed random w in code
    
    ---------
    
    Co-authored-by: noahthegreatz <noahsharp01@gmail.com>
    Co-authored-by: Nicholas Ford <nicholasford98@yahoo.com>

--- world/dungeon/exitroom/exit_1.gd
+++ world/dungeon/exitroom/exit_1.gd
@@ -3,4 +3,4 @@ extends game_controller
 func _on_exit_body_entered(body: Node2D) -> void:
 	print("entered... ready to exit")
 	Global.tokens = Global.tokens + 1
-w	get_tree().change_scene_to_file(Global.room_order.pop_front())
+	get_tree().change_scene_to_file(Global.room_order.pop_front())
